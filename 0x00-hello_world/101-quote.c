efdf3518e9df0a55633f2580a9ddb95f1b091480
muubaraq <sixthlogic@gmail.com>
2022-03-10 22:31:47 +0100

    at last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - print quote
- * Description: Dora korpa's quote
- * Return: Always 1 (Success)
+ * main - Entry point
+ * Return: return 1
  */
 
 int main(void)
-
 {
-
-write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-
 }
