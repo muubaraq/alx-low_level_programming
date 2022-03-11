0064e7f93dc1dc697022cef3b4469946065631b2
Mubaraq <58139049+muubaraq@users.noreply.github.com>
2022-03-11 16:58:04 +0100

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-
 /**
  * main - print double digit combos
  *
@@ -10,63 +7,34 @@
  * Return: Always 0 (Success)
  */
 
-
-
 int main(void)
-
 {
 	int i, j;
 
-
-
 	i = 48;
-
 	j = 48;
 
-
-
 	while (i < 58)
-
 	{
-
 		j = i + 1;
 
 		while (j < 58)
-
 		{
-
 			putchar(i);
-
 			putchar(j);
-
-
-
+			
 			if (i < 56 || j < 57)
-
 			{
-
 				putchar(44);
-
 				putchar(32);
-
 			}
 
 			j++;
-
 		}
 
-
-
 		i++;
-
 	}
 
-
-
 	putchar(10);
-
-
-
 	return (0);
-
 }
