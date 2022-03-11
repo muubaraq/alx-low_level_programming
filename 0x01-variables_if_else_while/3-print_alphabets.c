2e0574aae8202eab57bf4363fe0d851ea777d2a3
mubaraq <sixthlogic@gmail.com>
2022-03-11 07:29:24 -0800

    printalphy

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 /**
  * main - print the letters of the alphabet
  *
@@ -8,22 +10,49 @@
  * Return: Always 0 (Success)
  */
 
+
+
 int main(void)
+
 {
 	int i, j;
+
+
+
 	i = 97;
+
 	j = 65;
 
+
+
 	while (i < 123)
+
 	{
+
 		putchar(i);
+
 		i++;
+
 	}
+
+
+
 	while (j < 91)
+
 	{
+
 		putchar(j);
+
 		j++;
+
 	}
-	putchar(10\n);
+
+
+
+	putchar(10);
+
+
+
 	return (0);
+
 }
