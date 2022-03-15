f412d803a6132dc165cd028c5ddf1c29769fd6a0
muubaraq <sixthlogic@gmail.com>
2022-03-15 17:34:43 +0100

    ..

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,10 +12,8 @@ int main(void)
 	int i = 0;
 
 	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
-	return (0);
+{
+	_putchar(text[i]);
+}	_putchar('\n');
+return (0);
 }   
