133cdd9407dac3bb0293a2f88bf0fa3b1c957d94
muubaraq <sixthlogic@gmail.com>
2022-03-15 17:50:20 +0100

    ...

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - prints _putchar
- * Description: prints _putchar
- * Return: 0 (Success)
- */
+* main - prints _putchar
+* Description: prints _putchar
+* Return: 0 (Success)
+*/
 int main(void)
 {
 char text[9] = "_putchar";
