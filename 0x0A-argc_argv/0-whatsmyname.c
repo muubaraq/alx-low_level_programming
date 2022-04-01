69d8a6b13591dfadcdfd119e53ac74036b49462c
muubaraq <sixthlogic@gmail.com>
2022-04-01 14:57:25 +0100

    advance

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
+#define UNUSED(x) (void)(x)
+
 /**
- * main - prints its name, followed by a new line.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
- */
-int main(int argc __attribute__((unused)), char *argv[])
+* main - function
+*
+* @argv: the chaine of chaine
+* @argc: the int
+* Return: Always 0.
+*/
+
+int main(int argc, char **argv)
 {
+	UNUSED(argc);
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
